e8f2bc6746db66d8b23ac055f1aa3007e8ab2564
Justin Kirk <justinleekirk@gmail.com>
2020-12-08 13:59:26 -0600

    Finished

--- Bean Counting/Bean Counting.js
+++ Bean Counting/Bean Counting.js
@@ -1,1 +1,17 @@
-function countBs
+function countBs(word){
+    let totalLetter = 0;
+    for (count = 0; count < word.length; count++) {
+        if (word[count] === "B") totalLetter++;
+    }
+}//Count how man Bs are in a word.
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+function countChar(word, letter){
+    let totalLetter = 0;
+    for (count = 0; count < word.length; count++) {
+        if (word[count] === letter) totalLetter++;
+    }
+    return totalLetter
+}//Counts how many of a specified letter are in a word.

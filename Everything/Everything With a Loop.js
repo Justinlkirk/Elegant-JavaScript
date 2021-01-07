4f25779cf7a3e9e9c24590fb650bad0d0d0c6147
Justin Kirk <justinleekirk@gmail.com>
2021-01-07 17:27:25 -0600

    The loop version of everything

---
+++ Everything/Everything With a Loop.js
@@ -0,0 +1,13 @@
+function every(array, test) {
+    for (let number of array){
+        if (test(number)) return true;
+    }
+    return false;
+  }
+  
+  console.log(every([1, 3, 5], n => n < 10));
+  // → true
+  console.log(every([2, 4, 16], n => n < 10));
+  // → false
+  console.log(every([], n => n < 10));
+  // → true
